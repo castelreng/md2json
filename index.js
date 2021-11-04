ed6fe79ffd90138ba288d8422a82cc86a886ca7c
Joao Paulo Santos Batista Braga <joaopaulo.x.santosbatistabraga@gsk.com>
2021-11-04 10:58:38 +0100

    Add readme

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ import express from 'express';
 import { fromMarkdown } from 'mdast-util-from-markdown';
 import { toMarkdown } from 'mdast-util-to-markdown';
 import { readFileSync } from 'fs';
-import { remark } from 'remark';
 
 const app = express()
 const port = 3000
-
-/**
- * Use of mdast-util-from-markdown & mdast-util-to-markdown
- */
 
 app.get('/getJsonTreeFromMD', (req, res) => {
     const markdownContent = readFileSync("./example.md");
@@ -24,26 +19,6 @@ app.get('/getMDFromJsonTree', (req, res) => {
     res.setHeader('Content-type', "application/octet-stream");
     res.setHeader('Content-disposition', 'attachment; filename=example.md');
     res.send(markdownDisplay);
-});
-
-/** 
- * Use of remark
- */
-
-app.get('/remark/getJsonTreeFromMD', (req, res) => {
-  const markdownContent = readFileSync("./example.md");
-  const markdownTree = remark.parse(markdownContent);
-  res.json(markdownTree)
-});
-
-
-app.get('/remark/getMDFromJsonTree', (req, res) => {
-  const markdownContent = readFileSync("./example.md");
-  const markdownTree = remark.parse(markdownContent);
-  const markdownDisplay = remark.stringify(markdownTree);
-  res.setHeader('Content-type', "application/octet-stream");
-  res.setHeader('Content-disposition', 'attachment; filename=example.md');
-  res.send(markdownDisplay);
 });
 
 app.listen(port, () => {
---
+++ readme.md
@@ -0,0 +1,19 @@
+## Install
+
+[npm][]:
+
+```sh
+npm install 
+```
+
+## How to test
+
+```sh
+npm start
+````
+
+### Endpoints available
+```
+- http://localhost:3000/getJsonTreeFromMD
+- http://localhost:3000/getMDFromJsonTree
+```
